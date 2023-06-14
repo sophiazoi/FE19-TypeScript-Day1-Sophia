fbf826f77557ea2aa03937e7ca52d032fc0c3b9e
sophiazoi <133604009+sophiazoi@users.noreply.github.com>
2023-06-14 09:52:03 +0200

    Exercise 2

---
+++ Basic-Exercise2/Exercise2.ts
@@ -0,0 +1,22 @@
+// Task 1: Print full name in the browser
+const fullName = {
+  fName: "Sophia",
+  lName: "Saliba",
+};
+document.write(`Full Name: ${fullName.fName} ${fullName.lName}<br>`);
+
+// Task 2: Print first name 10 times using forEach loop
+const nameArray: { fName: string; lName: string }[] = [fullName];
+nameArray.forEach((name, index) => {
+  document.write(`First Name (${index + 1}): ${name.fName}<br>`);
+});
+
+// Task 3: Function triggered after 5 seconds to print last name in the console
+function printLastName() {
+  const lastName = fullName.lName;
+  for (let i = 0; i < 10; i++) {
+    console.log(`Last Name (${i + 1}): ${lastName}`);
+  }
+}
+
+setTimeout(printLastName, 5000);
