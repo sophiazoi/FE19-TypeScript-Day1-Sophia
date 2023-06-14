a90778c992ab2a284c1f5f6590ec80b2a4b82e76
sophiazoi <133604009+sophiazoi@users.noreply.github.com>
2023-06-14 09:55:47 +0200

    Exercise3

---
+++ Basic-Exercise3/Exercise3.ts
@@ -0,0 +1,16 @@
+const names: string[] = ["Alice", "Bob", "Charlie", "David"];
+
+// Task 1: Print only the index of the elements
+for (let i = 0; i < names.length; i++) {
+  console.log(`Index: ${i}`);
+}
+
+// Task 2: Print only the values
+for (const name of names) {
+  console.log(`Value: ${name}`);
+}
+
+// Task 3: Print both index and values
+for (const [index, name] of names.entries()) {
+  console.log(`Index: ${index}, Value: ${name}`);
+}
